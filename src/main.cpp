6b638a722357ec450a1d33d6dd63d46a119161b3
JCOno-dit-Biot <jc.onoditb@gmail.com>
2024-11-04 10:34:40 -0500

    send data over HTTP instead of BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,120 +1,138 @@
 #include <Arduino.h>
 #include "Adafruit_Si7021.h"
 #include "SPI.h"
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
-
-
-BLECharacteristic *pCharacteristic;
-
-//Boolean that says if the device is connected or not
-bool deviceConnected= false;
-
-// float to store temperature reading
-float Temp= 0;
-
-//Define the UUID for service and characteristics
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-
+#include "WiFi.h"
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include "wifiConfig.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "time.h"
 
 //Define constante for Deep sleep mode
-
 #define uS_TO_S_CONVERSION 1000000
-#define DEEP_SLEEP_TIME 10 // in sec
+#define DEEP_SLEEP_TIME 600 // in sec
 
-void device_sleep(int seconds){
-   
+void device_deep_sleep(int seconds){
     delay(500);
     esp_sleep_enable_timer_wakeup(seconds * uS_TO_S_CONVERSION);
     esp_deep_sleep_start();
 }
 
-
-
-class serverCallbacks: public BLEServerCallbacks{
-    void onConnect(BLEServer* pServer){
-        deviceConnected=true;
-    };
-
-    void onDisconnect(BLEServer* pServer){
-        deviceConnected=false;
-    };
-
-
-};
-
-
 Adafruit_Si7021 sensor = Adafruit_Si7021();
+
+int serialNumber = 100;
+
+time_t getUnixTimestamp() {
+    struct tm timeinfo;
+    int retryCount = 0;
+    while (!getLocalTime(&timeinfo)) {
+        retryCount++;
+        if (retryCount > 5) {
+            Serial.println("Failed to obtain time after multiple retries");
+            return 0;
+        }
+        delay(1000); // Wait for 1 second before retrying
+    }
+    time_t unixTime = mktime(&timeinfo);
+    return unixTime;
+}
+
+
+void sendJson(float temperature, float humidity) {
+    HTTPClient http;
+
+    String apiEndpoint = String(apiEndpointBase) + "/api/measurement";
+    http.begin(apiEndpoint.c_str()); //Specify request destination
+    http.addHeader("Content-Type", "application/json");
+
+    // Prepare the HTTP POST request data
+    // Create a JSON document
+    JsonDocument doc;
+    doc["temperature"] = temperature;
+    doc["humidity"] = humidity/100;
+    doc["sensor_id"] = serialNumber;
+
+    // TODO: debug getUnixTimestamp, getUniTimestamp keeps failing even with retries and return 0
+    // time_t timestamp =getUnixTimestamp();
+
+    // If the timestamp is not 0, then it is added to the json object
+    // if (timestamp != 0) {
+    //     Serial.println("timestamp: " + String(timestamp));
+    //     doc["timestamp"] = timestamp;
+    // }
+    
+    // Serialize the json object
+    String json;
+    serializeJson(doc, json);
+    Serial.print(json);
+
+    // Post the json object to the api endpoint
+    int httpResponseCode = http.POST(json);
+
+    // Error handling
+    if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+    } else {
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end();  //Free resources
+}
 
 
 void setup (){
+
     Serial.begin(115200);
 
-    sensor.begin();
+    // Initialize the Si7021 sensor
+    if (!sensor.begin()) {
+    Serial.println("Could not find a valid Si7021 sensor, check wiring!");
+    while (1);
+    }
 
-    // Create the BLE Device
-    BLEDevice::init("ESP32");
+    // Connect to Wi-Fi
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting to WiFi..");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi..");
+    }
 
-    // Create the BLE Server
-    BLEServer *pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new serverCallbacks());
-
-    // Create the BLE Service
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-
-    // Create the BLE Characteristic
-    pCharacteristic = pService->createCharacteristic(
-                        CHARACTERISTIC_UUID,
-                        BLECharacteristic::PROPERTY_NOTIFY
-    );
-
-    //BLE2902 needed to notify
-    pCharacteristic->addDescriptor(new BLE2902());
-
-    //Start the service
-    pService-> start();
-
-    //start advertising 
-    pServer->getAdvertising()->start();
-    Serial.println("Waiting for a client connection to notify...");
-
-
-
+    Serial.println("Connected to the WiFi network");
+    
 }
+
 void loop (){
 
-    delay(5000);
+    float temperature = sensor.readTemperature(); // Read temperature from Si7021
+    float humidity = sensor.readHumidity(); // Read Humidity from Si7021
 
-    if (deviceConnected){
-        Temp=sensor.readTemperature();
+    //printing for debugging only
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
 
-        // Conversion of Temp
-        char txString[8];
-        dtostrf(Temp,2,2,txString);
+    // Check WiFi connection status
+    if(WiFi.status() == WL_CONNECTED){
 
-        //Setting the value to the characteristic
-        pCharacteristic->setValue(txString);
+        sendJson(temperature, humidity);
 
-        //Notify the connected client
-        pCharacteristic->notify();
+    } else {
+        Serial.println("WiFi Disconnected. Attempting reconnection...");
+        WiFi.disconnect();
+        WiFi.reconnect();
+    }
 
-        Serial.println("Sent value: "+String(txString));
+    //put the device in deep sleep mode for 10min
+    device_deep_sleep(DEEP_SLEEP_TIME);
 
-        Serial.println("Going to deep sleep");
-        device_sleep(DEEP_SLEEP_TIME);
-    };
-
-
-    if (!deviceConnected){
-        
-        Serial.println("No connection detected, going back to sleep");
-        device_sleep(DEEP_SLEEP_TIME);
-    };
-
-   
 }
 
 
